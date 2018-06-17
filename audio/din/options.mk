d5d5f10cac44fd438bcbdd03940b710284170bf2
fraggerfox <santhosh.raju@gmail.com>
2018-06-18 01:08:16 +0530

    Pulled in upstream changes by wiz from pkgsrc-git.

--- audio/din/options.mk
+++ audio/din/options.mk
@@ -7,15 +7,17 @@ PKG_SUGGESTED_OPTIONS=	jack
 .include "../../mk/bsd.options.mk"
 
 ###
-### Support JACK
-###
-.if !empty(PKG_OPTIONS:Mjack)
-CXXFLAGS+=		-D__UNIX_JACK__
-.endif
-
-###
 ### Support ALSA
 ###
 .if !empty(PKG_OPTIONS:Malsa)
 CXXFLAGS+=		-D__LINUX_ALSA__
+.include "../../audio/alsa-lib/buildlink3.mk"
+.endif
+
+###
+### Support JACK
+###
+.if !empty(PKG_OPTIONS:Mjack)
+CXXFLAGS+=		-D__UNIX_JACK__
+.include "../../audio/jack/buildlink3.mk"
 .endif
